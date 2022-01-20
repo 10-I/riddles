81744aad20f419cd9d1d36550527bb6f1952315a
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2022-01-20 13:07:49 +0400

    basically fixed one of the most requested thingy

--- js/script.js
+++ js/script.js
@@ -1,64 +1,21 @@
-let x = Math.floor((Math.random() * 13) + 1);
-if (x == 1) {
-    document.getElementById("get").innerHTML = "What is always in front of you but can’t be seen? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 2) {
-    document.getElementById("get").innerHTML = "What can you break, even if you never pick it up or touch it? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 3) {
-    document.getElementById("get").innerHTML = "What can you catch, but not throw? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 4) {
-    document.getElementById("get").innerHTML = "I’m tall when I’m young, and I’m short when I’m old. What am I? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 5) {
-    document.getElementById("get").innerHTML = "The more of this there is, the less you see. What is it? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 6) {
-    document.getElementById("get").innerHTML = "What can fill a room but takes up no space? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 7) {
-    document.getElementById("get").innerHTML = "If you drop me I’m sure to crack, but give me a smile and I’ll always smile back. What am I? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 8) {
-    document.getElementById("get").innerHTML = "What has one head, and four legs? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 9) {
-    document.getElementById("get").innerHTML = "What gets wet while drying? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 10) {
-    document.getElementById("get").innerHTML = "What kind of room has no doors or windows? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 11) {
-    document.getElementById("get").innerHTML = "Poor people have it. Rich people need it. If you eat it you die. What is it? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 12) {
-    document.getElementById("get").innerHTML = "If your uncle’s sister is not your aunt, what relation is she to you? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else if (x == 13) {
-    document.getElementById("get").innerHTML = "Sara has four daughters, and each of her daughters has a brother. How many children does Sara have? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>"
-}
-else {
-    alert("Something went wrong")
-}
+let x = Math.floor((Math.random() * 7) + 0);
+let QAPair = [
+    { Q: "What is always in front of you but can’t be seen? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>", A: "Future" },
+    { Q: "What can you break, even if you never pick it up or touch it? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>", A: "Promise" },
+    { Q: "What can you catch, but not throw? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>", A: "Cold" },
+    { Q: "I’m tall when I’m young, and I’m short when I’m old. What am I? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>", A: "Candle" },
+    { Q: "The more of this there is, the less you see. What is it? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>", A: "Darkness" },
+    { Q: "What can fill a room but takes up no space? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>", A: "Light" },
+    { Q: "If you drop me I’m sure to crack, but give me a smile and I’ll always smile back. What am I? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>", A: "Mirror" },
+    { Q: "What has one head, and four legs? <br> <input type='text' placeholder='Type your answer' id='answer'  ><br><button class='button-28' onclick='submit()'>Submit Answer</button>", A: "Bed" }
+]
+document.getElementById("get").innerHTML = QAPair[x].Q
 function submit() {
     let z = document.getElementById("answer").value
-    if (x == 1 && z == "Future" ||
-        x == 2 && z == "Promise" ||
-        x == 3 && z == "Cold" ||
-        x == 4 && z == "Candle" ||
-        x == 5 && z == "Darkness" ||
-        x == 6 && z == "Light" ||
-        x == 7 && z == "Mirror" ||
-        x == 8 && z == "Bed" ||
-        x == 9 && z == "Towel" ||
-        x == 10 && z == "Mushroom" ||
-        x == 11 && z == "Nothing" ||
-        x == 12 && z == "Mother" ||
-        x == 13 && z == "Five") {
+    if (QAPair[x].A == z) {
         document.getElementById("get").innerHTML = "Your answer is correct!!<br> <img src='./src/Great-Job-Emoji-PNG-File.png'>"
     }
     else {
-        document.getElementById("get").innerHTML = "Your answer is incorrect!!<br> <img src='./src/pngegg.png'>"
+        document.getElementById("get").innerHTML = `Your answer is incorrect!! <br> Answer: ${QAPair[x].A}<br> <img src='./src/pngegg.png'>`
     }
 }
